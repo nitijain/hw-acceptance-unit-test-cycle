5af86bb4d66bae73305e01a30e5013b1b6729185
EC2 Default User <ec2-user@ip-172-31-38-100.us-east-2.compute.internal>
2018-10-05 06:01:38 +0000

    Part 1: add a Director field to Movies

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,5 +1,21 @@
 Rottenpotatoes::Application.routes.draw do
-  resources :movies
-  # map '/' to be a redirect to '/movies'
-  root :to => redirect('/movies')
+  resources :movies do
+    member do
+        get 'show_directors'
+    end
+  root :to => 'movies#index'
 end
+end
+
+
+
+
+#Rottenpotatoes::Application.routes.draw do
+#  resources :movies
+# do
+  # member do
+#	get 'show_directors'
+ #  end
+  # map '/' to be a redirect to '/movies'
+ # root :to => redirect('/movies')
+#end
