b37732b8f15eabb78c5b5e6e3d691d638990d045
EC2 Default User <ec2-user@ip-172-31-38-100.us-east-2.compute.internal>
2018-10-05 05:54:46 +0000

    Part 1: add a Director field to Movies

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -1,7 +1,17 @@
 class MoviesController < ApplicationController
   
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
+  end
+
+  def show_directors
+    @movie = Movie.find(params[:id])
+    if @movie.director.blank?
+      flash[:notice] = "'#{@movie.title}' has no director info"
+      redirect_to movies_path
+    else
+      @movies = Movie.where(director: @movie.director)
+    end
   end
 
   def show
